97d88602a32c199e72ed87eca05b6a9c153bed66
refonando <rnganteng98@gmail.com>
2025-03-08 16:50:14 +0700

    banyak belajar

--- eventobject/app.js
+++ eventobject/app.js
@@ -1,25 +1,28 @@
-document.querySelector('button').addEventListener('click', (e) => {
-	console.log(e);
+document.querySelector("button").addEventListener("click", (e) => {
+  console.log(e);
 });
 
-const input = document.querySelector('input');
-input.addEventListener('keydown', (e) => {
-	switch (e.code) {
-		case 'ArrowUp':
-			console.log('Tombol Arah Atas');
-			break;
-		case 'ArrowDown':
-			console.log('Tombol Arah Bawah');
-			break;
-		case 'ArrowLeft':
-			console.log('Tombol Arah Kiri');
-			break;
-		case 'ArrowRight':
-			console.log('Tombol Arah Kanan');
-			break;
-		default:
-			console.log('diabaikan');
-	}
+const input = document.querySelector("input");
+input.addEventListener("keydown", (e) => {
+  switch (e.code) {
+    case "ArrowUp":
+      console.log("Tombol Arah Atas");
+      break;
+    case "ArrowDown":
+      console.log("Tombol Arah Bawah");
+      break;
+    case "ArrowLeft":
+      console.log("Tombol Arah Kiri");
+      break;
+    case "ArrowRight":
+      console.log("Tombol Arah Kanan");
+      break;
+    case "KeyE":
+      console.log("Tombol Huruf E");
+      break;
+    default:
+      console.log(`tombol yg kamu pencet adalah ${e.code}`);
+  }
 });
 
 // input.addEventListener('keyup', () => {
--- eventprevent/app.js
+++ eventprevent/app.js
@@ -1,12 +1,18 @@
-const form = document.querySelector('#form');
-const input = document.querySelector('input');
-const list = document.querySelector('#notes');
+const form = document.querySelector("#form");
+const input = document.querySelector("input");
+const list = document.querySelector("#notes");
+const catat = document.querySelector("#catatan");
 
-form.addEventListener('submit', function (e) {
-	e.preventDefault();
-	const noteValue = input.value;
-	const newList = document.createElement('li');
-	newList.innerText = noteValue;
-	list.append(newList);
-	input.value = '';
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const noteValue = input.value;
+  const newList = document.createElement("li");
+  const newCatat = document.createElement("li");
+
+  newList.innerText = noteValue;
+  newCatat.innerText = noteValue;
+  list.appendChild(newList);
+  catat.appendChild(newCatat);
+
+  input.value = "";
 });
